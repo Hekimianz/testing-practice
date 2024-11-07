d354ab7448473730c9ca6c6685c16d03dbdfdc17
Aram Hekimian <guerrerohek121@gmail.com>
2024-11-07 12:40:29 -0600

    Finished calculator, caesar cipher, and analyze array functions and test suites

--- app.js
+++ app.js
@@ -10,4 +10,90 @@ function reverseString(string) {
   return result;
 }
 
-module.exports = { capitalize, reverseString };
+const calculator = {
+  sum(a, b) {
+    return a + b;
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
+  divide(a, b) {
+    return a / b;
+  },
+};
+
+function caesarCipher(s, n) {
+  const alph = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  const splitWord = s.split("");
+  let result = "";
+
+  splitWord.forEach((letter) => {
+    const index = alph.indexOf(letter.toLowerCase());
+
+    if (index >= 0) {
+      const newIndex = (index + n) % 26;
+      result +=
+        letter.toUpperCase() === letter
+          ? alph[newIndex].toUpperCase()
+          : alph[newIndex];
+    } else {
+      result += letter;
+    }
+  });
+
+  return result;
+}
+
+function analyzeArray(arr) {
+  const total = arr.reduce((prev, curr) => {
+    return prev + curr;
+  });
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  const length = arr.length;
+  const average = total / length;
+  return {
+    average,
+    min,
+    max,
+    length,
+  };
+}
+
+module.exports = {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+};
--- app.test.js
+++ app.test.js
@@ -1,4 +1,10 @@
-const { capitalize, reverseString } = require("./app");
+const {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} = require("./app");
 
 // capitalize
 test("Capitalize: Works with single word strings.", () => {
@@ -32,4 +38,55 @@ test("reverseString: Works with multiple word strings.", () => {
 
 test("reverseString: Works with multiple word strings.", () => {
   expect(reverseString("Goodbye World")).toBe("dlroW eybdooG");
+});
+
+// Calculator
+test("Calculator: sum", () => {
+  expect(calculator.sum(1, 99)).toBe(100);
+});
+
+test("Calculator: subtract", () => {
+  expect(calculator.subtract(100, 1)).toBe(99);
+});
+
+test("Calculator: multiply", () => {
+  expect(calculator.multiply(5, 5)).toBe(25);
+});
+
+test("Calculator: divide", () => {
+  expect(calculator.divide(100, 2)).toBe(50);
+});
+
+//Caesar Cipher
+test("Caesar: shifts up", () => {
+  expect(caesarCipher("abc", 1)).toBe("bcd");
+});
+
+test("Caesar: perserves case", () => {
+  expect(caesarCipher("Abc", 1)).toBe("Bcd");
+});
+
+test("Caesar: perserves punctuation", () => {
+  expect(caesarCipher("abc!", 1)).toBe("bcd!");
+});
+
+test("Caesar: wraps around", () => {
+  expect(caesarCipher("xyz", 3)).toBe("abc");
+});
+
+// analyze array
+test("Analyze: avg", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).average).toBe(4);
+});
+
+test("Analyze: min", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).min).toBe(1);
+});
+
+test("Analyze: max", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).max).toBe(8);
+});
+
+test("Analyze: length", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).length).toBe(6);
 });
